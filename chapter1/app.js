85ada7ef424f3451faa3f585838b8e26378f50f9
MANIA GHOURI <maniaghouri4@gmail.com>
2026-07-21 04:44:57 +0100

    chapter1,2

---
+++ chapter1/app.js
@@ -0,0 +1,15 @@
+// // QUESTION1
+// alert("WELCOME");
+// // QUESTION2
+// alert("Error! Please enter a valid password.");
+// // QUESTION3
+// alert("Welcome to JS Land...\nHappy Coding!");
+// // QUESTION4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// // QUESTION5
+// alert("Hello... I can run JS through my web browser's console");
+// // QUESTION6
+
+
---
+++ chapter2/app.js
@@ -0,0 +1,40 @@
+// QUESTION NO 1
+var username = "Mania ghouri";
+console.log(username);
+// QUESTION NO 2
+username = "Mania Khalid";
+console.log(username);
+username = "MANIA";
+console.log(username);
+// QUESTION NO 3
+// A
+var message = "HELLO WORLD";
+// B
+console.log(message);
+// C
+alert("JavaScript is a programming language");
+// QUESTION NO 4
+// Saving student's bio data in JS variables
+var studentName = "Johne Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+// Displaying the data in alert boxes
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+// QUESTION NO 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+var alert = "PIZZA";
+console.log(alert);
+// QUESTION NO 6
+var email = "maniaghouri4@gmail.com";
+console.log(email);
+// alert("My email address is" example@example.com ) ;
+// QUEATION NO 7
+var book = "A smarter way to learn JavaScript";
+console.log(book)
+alert("I am learning from the book smarter way to learn javascript");
+// QUESTION NO 8
+document.write("Yah! I can write HTML content through JavaScript");
+// QUESTION NO 9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
